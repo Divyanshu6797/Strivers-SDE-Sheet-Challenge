1c06d5eca570093d3c11f8bff3ce9dc8a60b6f30
Divyanshu6797 <117548669+Divyanshu6797@users.noreply.github.com>
2023-06-22 21:27:13 +0530

    Create find unique CN.cpp

---
+++ find unique CN.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+
+
+int findUnique(int *arr, int size)
+{
+    
+    int ans=0;
+
+    for(int i=0;i<size;i++)
+    {
+        ans=ans^arr[i];
+       
+
+    }
+    return ans;
+
+   
+
+
+}
